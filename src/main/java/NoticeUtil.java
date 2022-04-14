7bbfe957bac4d69b69c84a0f7b418673c202b205
mengge liao <liaomengge@gmail.com>
2022-04-14 20:07:20 +0800

    feat: small change

--- src/main/java/NoticeUtil.java
+++ src/main/java/NoticeUtil.java
@@ -1,5 +1,10 @@
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.google.common.collect.ImmutableMap;
+
+import java.util.Objects;
 
 /**
  * Created by liaomengge on 2022/04/14.
@@ -10,9 +15,18 @@ public class NoticeUtil {
 
     private static final String url = String.format("http://dd.100vs.com/api/%s/send", token);
 
-    public static void send() {
+    public static boolean send() {
         try {
-            HttpUtil.get(url, ImmutableMap.of("title", "下单成功", "content", "下单成功，快去支付吧。。。"), 3000);
-        } catch (Exception e) {}
+            String result = HttpUtil.get(url, ImmutableMap.of("title", "下单成功", "content", "下单成功，快去支付吧。。。"), 3000);
+            if (StrUtil.isNotBlank(result)) {
+                JSONObject jsonObject = JSONUtil.parseObj(result);
+                if (Objects.nonNull(jsonObject)) {
+                    return StrUtil.equals("200", jsonObject.getStr("status"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
